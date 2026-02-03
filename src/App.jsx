02380d03a2a3fc2c7ce5629dc1cdead27d48ea22
alex Kochens <ofwonoalex256@gmail.com>
2026-02-03 10:57:23 +0300

    feat: add local storage persistence

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import ItemForm from './components/ItemForm';
 import ItemList from './components/itemList';
 
 const App = () => {
-  const [item, setItem] = useState([]);
+  const [item, setItem] = useState(() => {
+    const item = JSON.parse(localStorage.getItem('item'));
+    return item || [];
+  });
+
+  //local storage persistence
+  useEffect(() => {
+    localStorage.setItem('item', JSON.stringify(item));
+  }, [item]);
 
   const addItem = (item) => {
     setItem((prev) => [
@@ -19,6 +27,7 @@ const App = () => {
   const deleteItem = (id) => {
     setItem(item.filter((i) => i.id !== id));
   };
+
   return (
     <div>
       <ItemForm onAddItem={addItem} />
