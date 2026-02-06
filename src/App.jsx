f9b82ff1b38183c0dc2b9da7d56b113088e0a6df
alex Kochens <ofwonoalex256@gmail.com>
2026-02-06 11:39:01 +0300

    chore: fix search form rendering

--- src/App.jsx
+++ src/App.jsx
@@ -85,6 +85,7 @@ const App = () => {
         onSearch={onSearch}
       />
       <ItemList
+        search={search}
         item={searchedItems}
         onDeleteItem={deleteItem}
         onEditItem={editItem}
